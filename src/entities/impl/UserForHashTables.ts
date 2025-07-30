92403cde15a7c8b610d3b86c649c66bc9db143c0
mtsonkova <mtsonkova950@gmail.com>
2025-07-30 22:45:38 +0300

    Refactored all files from entities

--- src/entities/impl/UserForHashTables.ts
+++ src/entities/impl/UserForHashTables.ts
@@ -1,116 +1,96 @@
-//todo
+import {User} from '@src/entities/entitiesInterfaces/User';
+import hash from 'object-hash';
 
-// package com.itbulls.learnit.javacore.exam.solution.enteties.impl;
 
-// import java.util.Objects;
-
-// import com.itbulls.learnit.javacore.exam.solution.enteties.User;
-
-// public class UserForHashTables implements User {
+export class UserForHashTables implements User {
 	
-// 	private static int userCounter = 0;
+	private static userCounter: number = 0;
 	
-// 	private int id;
-// 	private String firstName;
-// 	private String lastName;
-// 	private String password;
-// 	private String email;
+	private id: number;
+	private firstName: string;
+	private lastName: string;
+	private password: string;
+	private email: string;
 
-// 	{
-// 		id = ++userCounter;
-// 	}
+		
+	public constructor(id: number, firstName: string, lastName: string, password: string, email: string) {
+		this.id = id ?? ++UserForHashTables.userCounter;
+        UserForHashTables.userCounter--; // to keep sequantial id
+        this.firstName = firstName ?? '';
+		this.lastName = lastName ?? '';
+		this.password = password ?? '';
+		this.email = email ?? '';
+	}
+
+	public getFirstName(): string {
+		return this.firstName;
+	}
+
+	public getLastName(): string {
+		return this.lastName;
+	}
+
+	public getPassword(): string {
+		return this.password;
+	}
+
+	public getEmail(): string {
+		return this.email;
+	}
 	
-// 	public UserForHashTables() {
-// 	}
+	public toString(): string {
+		return "ID: " + this.getId() + "\t\t" +
+				"First Name: " + this.getFirstName() + "\t\t" +
+				"Last Name: " + this.getLastName() + "\t\t" +
+				"Email: " + this.getEmail();
+	}
+
+	public setPassword(password: string): void {
+		if (password == null) {
+			return;
+		}
+		this.password = password;
+	}
+
+	public setEmail(newEmail: string): void {
+		if (newEmail == null) {
+			return;
+		}
+		this.email = newEmail;
+	}
+
+	public getId(): number {
+		return this.id;
+	}
 	
-// 	public UserForHashTables(String firstName, String lastName, String password, String email) {
-// 		this.firstName = firstName;
-// 		this.lastName = lastName;
-// 		this.password = password;
-// 		this.email = email;
-// 	}
-	
-// 	public UserForHashTables(int id, String firstName, String lastName, String password, String email) {
-// 		this.id = id;
-// 		userCounter--; // to keep sequantial id
-// 		this.firstName = firstName;
-// 		this.lastName = lastName;
-// 		this.password = password;
-// 		this.email = email;
-// 	}
+	clearState(): void {
+		UserForHashTables.userCounter = 0;
+	}
 
-// 	@Override
-// 	public String getFirstName() {
-// 		return this.firstName;
-// 	}
+	// public hashCode(): number {
+	// 	return Objects.hash(this.email, this.firstName, this.id, this.lastName, this.password);
+	// }
 
-// 	@Override
-// 	public String getLastName() {
-// 		return this.lastName;
-// 	}
+    public hashCode(): string {
+  return hash({
+    email: this.email,
+    firstName: this.firstName,
+    id: this.id,
+    lastName: this.lastName,
+    password: this.password
+  });
+}
 
-// 	@Override
-// 	public String getPassword() {
-// 		return this.password;
-// 	}
+public equals(obj: any): boolean {
+  if (this === obj) return true;
+  if (!obj || !(obj instanceof UserForHashTables)) return false;
 
-// 	@Override
-// 	public String getEmail() {
-// 		return this.email;
-// 	}
-	
-// 	@Override
-// 	public String toString() {
-// 		return "ID: " + this.getId() + "\t\t" +
-// 				"First Name: " + this.getFirstName() + "\t\t" +
-// 				"Last Name: " + this.getLastName() + "\t\t" +
-// 				"Email: " + this.getEmail();
-// 	}
-
-// 	@Override
-// 	public void setPassword(String password) {
-// 		if (password == null) {
-// 			return;
-// 		}
-// 		this.password = password;
-// 	}
-
-// 	@Override
-// 	public void setEmail(String newEmail) {
-// 		if (newEmail == null) {
-// 			return;
-// 		}
-// 		this.email = newEmail;
-// 	}
-
-// 	@Override
-// 	public int getId() {
-// 		return this.id;
-// 	}
-	
-// 	void clearState() {
-// 		userCounter = 0;
-// 	}
-
-// 	@Override
-// 	public int hashCode() {
-// 		return Objects.hash(email, firstName, id, lastName, password);
-// 	}
-
-// 	@Override
-// 	public boolean equals(Object obj) {
-// 		if (this == obj)
-// 			return true;
-// 		if (obj == null)
-// 			return false;
-// 		if (getClass() != obj.getClass())
-// 			return false;
-// 		UserForHashTables other = (UserForHashTables) obj;
-// 		return Objects.equals(email, other.email)
-// 				&& Objects.equals(firstName, other.firstName) && Objects.equals(id, other.id)
-// 				&& Objects.equals(lastName, other.lastName)
-// 				&& Objects.equals(password, other.password);
-// 	}
+  return this.email === obj.email &&
+         this.firstName === obj.firstName &&
+         this.id === obj.id &&
+         this.lastName === obj.lastName &&
+         this.password === obj.password;
+}
 	
 	
-// }
+}
