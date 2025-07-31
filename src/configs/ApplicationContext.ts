e12ecedb42dd63a62dcd4612795e54c588d03151
mtsonkova <mtsonkova950@gmail.com>
2025-07-31 21:26:00 +0300

    Refactored Application context

--- src/configs/ApplicationContext.ts
+++ src/configs/ApplicationContext.ts
@@ -1,55 +1,60 @@
-//ToDo
 
+import {Cart} from '@src/entities/entitiesInterfaces/Cart';
+import {User} from '@src/entities/entitiesInterfaces/User';
+import { DefaultCart } from '@src/entities/impl/DefaultCart';
+import{Menu} from '@src/menu/Menu';
 
-// package com.itbulls.learnit.javacore.exam.solution.configs;
+export class ApplicationContext {
+	
+	private static instance: ApplicationContext;
+	
+	private loggedInUser: User | null = null;
+	private mainMenu: Menu | null = null;
+	private sessionCart: Cart | null = null;
+	
+   constructor(loggedInUser: User | null = null, mainMenu: Menu | null = null, sessionCart: Cart | null = null) {
+	this.loggedInUser = loggedInUser;
+	this.mainMenu = mainMenu;
+	this.sessionCart = sessionCart;
+}
 
-// import com.itbulls.learnit.javacore.exam.solution.enteties.Cart;
-// import com.itbulls.learnit.javacore.exam.solution.enteties.User;
-// import com.itbulls.learnit.javacore.exam.solution.enteties.impl.DefaultCart;
-// import com.itbulls.learnit.javacore.exam.solution.menu.Menu;
+	
+	public setLoggedInUser(user: User): void {
+		if (this.sessionCart != null) {
+			this.sessionCart.clear(); // we have to clear session cart when new user is logged in
+		}
+		this.loggedInUser = user;
+	}
+	
+	public getLoggedInUser(): User | null{
+		return this.loggedInUser;
+	}
+	
+	public setMainMenu(menu: Menu): void {
+		this.mainMenu = menu;
+	}
+	
+	public getMainMenu(): Menu | null{
+		return this.mainMenu;
+	}
 
-// public class ApplicationContext {
-	
-// 	private static ApplicationContext instance;
-	
-// 	private User loggedInUser;
-// 	private Menu mainMenu;
-// 	private Cart sessionCart;
-	
-// 	private ApplicationContext() {
-// 	}
-	
-// 	public void setLoggedInUser(User user) {
-// 		if (this.sessionCart != null) {
-// 			this.sessionCart.clear(); // we have to clear session cart when new user is logged in
-// 		}
-// 		this.loggedInUser = user;
-// 	}
-	
-// 	public User getLoggedInUser() {
-// 		return this.loggedInUser;
-// 	}
-	
-// 	public void setMainMenu(Menu menu) {
-// 		this.mainMenu = menu;
-// 	}
-	
-// 	public Menu getMainMenu() {
-// 		return this.mainMenu;
-// 	}
+	public static getInstance(): ApplicationContext {
+	if (!this.instance) {
+		throw new Error("ApplicationContext has not been initialized.");
+	}
+	return this.instance;
+}
 
-// 	public static ApplicationContext getInstance() {
-// 		if (instance == null) {
-// 			instance = new ApplicationContext();
-// 		}
-// 		return instance;
-// 	}
+public static initialize(loggedInUser: User, mainMenu: Menu, sessionCart: Cart): void {
+	if (!this.instance) {
+		this.instance = new ApplicationContext(loggedInUser, mainMenu, sessionCart);
+	}
+}
+	public getSessionCart(): Cart {
+		if (this.sessionCart == null) {
+			this.sessionCart = new DefaultCart();
+		}
+		return this.sessionCart;
+	}
 
-// 	public Cart getSessionCart() {
-// 		if (this.sessionCart == null) {
-// 			this.sessionCart = new DefaultCart();
-// 		}
-// 		return this.sessionCart;
-// 	}
-
-// }
+}
